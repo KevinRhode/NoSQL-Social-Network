aab72c0cff10f660de2cf7d27461fbb40795441c
Kevin Rhode <kevinrhode@hotmail.com>
2023-06-11 22:55:41 -0400

    add: changes to user model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const {Schema, model} = require('mongoose');
+const thoughtSchema = require('./Thought');
 
 //Schema to create User model
 const userSchema = new Schema(
@@ -19,6 +20,21 @@ const userSchema = new Schema(
                 },
                 message: props => `${props.value} is not a valid phone number!`
             },
-        }
+        },
+        thoughts: [thoughtSchema],
+        friends:[this],
+    },
+    {
+        toJSON:{
+            virtuals:true,
+        },
+        id:false,
     }
-)
+);
+const friendCount = userSchema.virtual('friendCount');
+//todo 
+
+
+const User = model('user',userSchema);
+
+module.exports = User;
